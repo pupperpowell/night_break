29e7744d9b2d5670f7a10196759818de41ac2129
George Powell <geohpowell@gmail.com>
2024-02-21 15:39:12 -0500

    completed church scraper and json file

--- lib/logic/parishes.dart
+++ lib/logic/parishes.dart
@@ -1,3 +1,6 @@
+import 'dart:convert';
+import 'dart:io';
+
 import 'package:http/http.dart' as http;
 import 'package:html/parser.dart' as parser;
 
@@ -9,7 +12,6 @@ class Parish {
   String city;
   String state;
   String zip;
-  String country;
   double latitude;
   double longitude;
 
@@ -21,87 +23,94 @@ class Parish {
     required this.city,
     required this.state,
     required this.zip,
-    required this.country,
     required this.latitude,
     required this.longitude,
   });
+
+  Map<String, dynamic> toJson() => {
+        'name': name,
+        'jurisdiction': jurisdiction,
+        'streetAddress': streetAddress,
+        'city': city,
+        'state': state,
+        'zip': zip,
+        'latitude': latitude,
+        'longitude': longitude,
+      };
 }
 
 Future<List<Parish>> scrapeChurches() async {
-  const url =
-      'https://www.assemblyofbishops.org/directories/parishes/?radius=500&search_address=170%20Washington%20St.%20Malden%2C%20MA%2002148&search_coordinates=(42.432795%2C%20-71.0725045)&search_error=OK&searchType=proximity';
+  const url = '';
   final response = await http.get(Uri.parse(url));
 
   if (response.statusCode == 200) {
     print('status code: 200');
     final document = parser.parse(response.body);
 
-    final parishes = document.querySelectorAll('.parish_info');
+    final parishes = document.querySelectorAll('.output_parish');
 
     return parishes.map((parish) {
       final titleElement = parish.querySelector('.parish_title');
       final title = titleElement?.text.trim() ?? '';
-      // empty title?
 
       final jurisdictionElement = parish.querySelector('.parish_jurisdiction');
       final jurisdiction = jurisdictionElement?.text.trim() ?? '';
 
-      final addressLines = parish.querySelectorAll('p');
-      final address = addressLines
-          .where((p) =>
-              p.text.trim().isNotEmpty && !p.text.contains('Show more info'))
-          .join(', ');
+      final streetAddressElement = parish.querySelector('.parish_address');
+      final streetAddress = streetAddressElement?.text.trim() ?? '';
 
-      final cityStateZip = addressLines.last.text.trim().split(',');
-      final city = cityStateZip.isNotEmpty ? cityStateZip[0].trim() : '';
-      final state = cityStateZip.length >= 2 ? cityStateZip[1].trim() : '';
-      final zip = cityStateZip.length >= 3 ? cityStateZip[2].trim() : '';
+      final cityElement = parish.querySelector('.parish_city');
+      final city = cityElement?.text.trim() ?? '';
 
-      const country = 'USA';
+      final stateElement = parish.querySelector('.parish_state');
+      final state = stateElement?.text.trim() ?? '';
+
+      final zipElement = parish.querySelector('.parish_zip');
+      final zip = zipElement?.text.trim() ?? '';
 
       final geocodeElement = parish.querySelector('.parish_geocode');
-      final isExact = geocodeElement?.text.trim() == 'exact';
+      // final isExact = geocodeElement?.text.trim() == 'exact';
 
       final latitudeElement = parish.querySelector('.parish_latitude');
-      final latitude = isExact
-          ? double.tryParse(latitudeElement?.text.trim() ?? '') ?? 0.0
-          : 0.0;
+      // final latitude = isExact
+      //     ? double.tryParse(latitudeElement?.text.trim() ?? '') ?? 0.0
+      //     : 0.0;
+      final latitude =
+          double.tryParse(latitudeElement?.text.trim() ?? '') ?? 0.0;
 
       final longitudeElement = parish.querySelector('.parish_longitude');
-      final longitude = isExact
-          ? double.tryParse(longitudeElement?.text.trim() ?? '') ?? 0.0
-          : 0.0;
-
-      // final moreInfoElement = parish.querySelector('.parish_more_info');
-      // final phoneElement = moreInfoElement?.querySelector('.parish_phone');
-      // final phone = phoneElement?.text.trim() ?? '';
-
-      // final faxElement = moreInfoElement?.querySelector('.parish_fax');
-      // final fax = faxElement?.text.trim() ?? '';
-
-      // final websiteElement =
-      //     moreInfoElement?.querySelector('.parish_website');
-      // final website = websiteElement?.text.trim() ?? '';
+      // final longitude = isExact
+      //     ? double.tryParse(longitudeElement?.text.trim() ?? '') ?? 0.0
+      //     : 0.0;
+      final longitude =
+          double.tryParse(longitudeElement?.text.trim() ?? '') ?? 0.0;
 
       return Parish(
           name: title,
           jurisdiction: jurisdiction,
-          streetAddress: address,
+          streetAddress: streetAddress,
           city: city,
           state: state,
           zip: zip,
-          country: country,
           latitude: latitude,
           longitude: longitude);
     }).toList();
   } else {
-    print('couldn\'t get data from website');
     throw Exception('Failed to fetch data from website');
   }
 }
 
 void main() async {
   final churches = await scrapeChurches();
-  // print out the first church in the churches list
-  print(churches.length);
+
+  // Encode the list of churches as JSON
+  final jsonData = jsonEncode(churches);
+
+  // Specify the filename to write the JSON data
+  const filename = 'churches.json';
+
+  // Write the JSON data to the file
+  await File(filename).writeAsString(jsonData);
+
+  print('Churches saved to $filename');
 }
