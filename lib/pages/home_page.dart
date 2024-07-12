cd4e33564b473c939b9f02b3d703c31e08ecadbb
George Powell <geohpowell@gmail.com>
2024-07-12 14:27:04 -0400

    beautiful WIP home screen

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -8,21 +8,38 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Center(
-          child: Column(
-            children: [
-              const SizedBox(height: 32.0),
-              const Text(
-                'Welcome to Night Break v0.9\nmade with love by George Powell',
-                textAlign: TextAlign.center,
-              ),
-              const SizedBox(height: 16.0),
-              Candle(created: DateTime.now(), scale: 1.0, owner: "nobody"),
-              const SizedBox(height: 32.0),
-              const Text('This app is still in development.'),
-              const ListItem(checked: true, text: 'candle stand'),
-              const ListItem(checked: false, text: 'profile'),
-            ],
+        child: Padding(
+          padding: const EdgeInsets.all(32.0),
+          child: Center(
+            child: Column(
+              children: [
+                const Text(
+                  'Welcome to Night Break v0.9\nmade with love by George Powell',
+                  textAlign: TextAlign.center,
+                ),
+                const SizedBox(height: 16.0),
+                Candle(created: DateTime.now(), scale: 1.0, owner: "nobody"),
+                const SizedBox(height: 32.0),
+                const Text(
+                  'This app is still in development. A small number of features are currently implemented or planned:',
+                  textAlign: TextAlign.center,
+                ),
+                const SizedBox(height: 16.0),
+                const ListItem(checked: true, text: 'candle stand'),
+                const ListItem(
+                    checked: false, text: 'anonymous prayer requests'),
+                const ListItem(checked: false, text: 'daily readings'),
+                const ListItem(
+                    checked: false, text: 'synaxarion (saints of the day)'),
+                const SizedBox(height: 16.0),
+                const Text(
+                  'After that, features will be added based on suggestions from the community.',
+                  textAlign: TextAlign.center,
+                ),
+                const SizedBox(height: 16.0),
+                const ListItem(checked: false, text: 'in-app feature requests'),
+              ],
+            ),
           ),
         ),
       ),
@@ -44,20 +61,35 @@ class _ListItemState extends State<ListItem> {
   bool isChecked = false;
 
   @override
+  void initState() {
+    super.initState();
+    isChecked = widget.checked;
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        Checkbox(
-          checkColor: Colors.white,
-          value: isChecked,
-          onChanged: (bool? value) {
-            setState(() {
-              isChecked = value!;
-            });
-          },
-        ),
-        Text(widget.text, style: Theme.of(context).textTheme.bodyLarge),
-      ],
+    return SizedBox(
+      height: 28.0,
+      child: Row(
+        children: [
+          const SizedBox(width: 16.0),
+          Checkbox(
+            checkColor: Colors.white,
+            value: isChecked,
+            onChanged: (bool? value) {
+              setState(() {
+                isChecked = value!;
+              });
+            },
+          ),
+          Text(widget.text,
+              // style: Theme.of(context).textTheme.bodyLarge,
+              style: const TextStyle(
+                fontSize: 16.0,
+                color: Color.fromARGB(255, 153, 188, 205),
+              )),
+        ],
+      ),
     );
   }
 }
